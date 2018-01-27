9625a75c31e2ad4aca93f76c2663a6593f4ce5b6
Arnyminer Z <arnyminer.z@gmail.com>
2018-01-27 21:23:32 +0100

    Just a check

--- WiSync.py
+++ WiSync.py
@@ -7,7 +7,7 @@ from elements.Panel import *
 window = Window(Vector2(700, 500), "Wi Sync", Colors.dark_gray)
 panel = HeaderPanel(Vector2(30, 30), Vector2(200, 180), "Panel Header")
 window.add(panel)
-button1 = Button("Button 1", Vector2(10, 10), Vector2(100, 30), print("Clicked btn 1"))
-window.add(button1)
+# button1 = Button("Button 1", Vector2(10, 10), Vector2(100, 30), print("Clicked btn 1"))
+# window.add(button1)
 
 window.start()
