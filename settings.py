96816021ec8bd6a11c857b5c222c77f38c4cc20f
Arnyminer Z <arnyminer.z@gmail.com>
2018-01-28 21:06:21 +0100

    Trying frameworks

---
+++ settings.py
@@ -0,0 +1,5 @@
+INSTALLED_APPS = (
+    'material',
+    'material.frontend',
+    'materialize'
+)
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['WiSync.py'])
+setup(console=['WiSync.py'], requires=['htmlPy'])
