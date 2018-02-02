7d48de94396dabf5c1debee0373525b0ef612032
ArnyminerZ <Arnyminer.Z@gmail.com>
2018-02-02 18:17:49 +0100

    Updated Interpreter and Windows Dependencies Loader

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,5 +1,7 @@
 @echo off
-if not exist "C:\Python34\python.exe" goto installpython
+if not exist "C:\Python34\python.exe" if not exist "C:\Users\Arnym\AppData\Local\Programs\Python\Python36-32\python.exe" (
+    goto installpython
+)
 echo Python already installed.
 goto installpygame
 
@@ -9,7 +11,11 @@ echo Installing Python 3.6.4
 SET downloadUrl=https://www.python.org/ftp/python/3.6.4/python-3.6.4.exe
 SET tempFile=%cd%\.%random%-tmp
 
+echo "Downloading..."
+
 BITSADMIN /transfer /download %downloadUrl% %tempFile% >nul
+
+echo "ok"
 
 start %tempFile%
 del %tempFile%
@@ -17,13 +23,34 @@ goto installpygame
 
 :installpygame
 echo Installing pygame
-python -m pip install pygame > null
-goto installpy2exe
+python -m pip install pygame > null && (
+    goto installpy2exe
+) || (
+if exist "C:\Python34\python.exe" (
+    C:\Python34\python.exe -m pip install pygame
+    goto installpy2exe
+)
+if exist "C:\Python34\python.exe" (
+    C:\Users\Arnym\AppData\Local\Programs\Python\Python36-32\python.exe -m pip install pygame
+    goto installpy2exe
+)
+)
 
 :installpy2exe
 echo Installing py2exe
-python -m pip install py2exe > null
-goto end
+python -m pip install py2exe > null && (
+    goto end
+) || (
+if exist "C:\Python34\python.exe" (
+    C:\Python34\python.exe -m pip install py2exe
+    goto end
+)
+if exist "C:\Python34\python.exe" (
+    C:\Users\Arnym\AppData\Local\Programs\Python\Python36-32\python.exe -m pip install py2exe
+    goto end
+)
+)
 
 :end
 echo Finished.
+PAUSE
