f25c88a7da95b68cef2c226696f3de3fdf03b387
ArnyminerZ <Arnyminer.Z@gmail.com>
2018-02-08 21:41:30 +0100

    Added run file

--- WiSync/__init__.py
+++ WiSync/__init__.py
@@ -2,7 +2,7 @@ from elements import *
 from Dimensions import *
 from Color import *
 
-window = Window(Size(1000, 700), "WiSync", "logo.png")
+window = Window(Size(1000, 700), "WiSync", None)
 
 panel = Panel(Position(10, 10), Size(160, 55), Colors.flat_red)
 # window.add(panel)
---
+++ run.bat
@@ -0,0 +1,2 @@
+@echo off
+python WiSync/__init__.py
