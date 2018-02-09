fa34e603bf59ca8576a348d80f252609c005ab4a
ArnyminerZ <Arnyminer.Z@gmail.com>
2018-02-09 10:09:46 +0100

    Create clone.bat

---
+++ clone.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Press any key for start cloning the repository.
+PAUSE > null
+echo Clonning...
+git clone https://github.com/CMakerA/WiSync.git > null
+echo Done. Now you can access the file in the folder WiSync.
+PAUSE
