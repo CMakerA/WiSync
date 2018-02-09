49d292ed703f78cfd802ca4fbcfcf9ab4efa8071
ArnyminerZ <Arnyminer.Z@gmail.com>
2018-02-09 15:34:05 +0100

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,10 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+command -v git >/dev/null 2>&1 || { echo >&2 "I require git but it's not installed. Installing..."; apt-get install git; }
+
 apt-get install python3 -y
 
 wget https://bootstrap.pypa.io/get-pip.py
